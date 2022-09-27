1f189a3181984986f94f5078898f99414e94723c
Wayne Tan <wtjh00@gmail.com>
2022-09-28 00:03:59 +0800

    feat: added searchbar for user to query

--- django_backend/views.py
+++ django_backend/views.py
@@ -5,11 +5,20 @@ def search(request):
     """
     User should only be able to change the searchVal attribute. The other attributes have to be finalised before deployment.
     """
-    searchVal = 'pasir'
     returnGeom = 'Y'
     getAddrDetails = 'Y'
     pageNum = 1
+
+    if 'searchVal' in request.GET:
+
+        searchVal = request.GET['searchVal']
+
+        url = 'https://developers.onemap.sg/commonapi/search?searchVal={searchVal}&returnGeom={returnGeom}&getAddrDetails={getAddrDetails}&pageNum={pageNum}'.format(searchVal=searchVal, returnGeom=returnGeom, getAddrDetails=getAddrDetails, pageNum=pageNum)
+        
+        response = requests.get(url=url)
     
-    response = requests.get('https://developers.onemap.sg/commonapi/search?searchVal={searchVal}&returnGeom={returnGeom}&getAddrDetails={getAddrDetails}&pageNum={pageNum}'.format(searchVal=searchVal, returnGeom=returnGeom, getAddrDetails=getAddrDetails, pageNum=pageNum))
-    results = response.json()
-    return render(request, 'pages/search.html', results)
+        searchResult = response.json()
+    else:
+        searchResult = {}
+
+    return render(request, 'pages/search.html', searchResult)
