52bde9916b04ec11b0c2c4fbf11912b6bf83b5b7
lameo125 <marcus.hooi@gmail.com>
2022-10-20 16:33:10 +0800

    Added responsive layout

--- src/components/GoogleMap.js
+++ src/components/GoogleMap.js
@@ -72,9 +72,30 @@ export class MapContainer extends Component {
                 
                 />
                 <div className="autocomplete-dropdown-container">
-                  <Cards/>
+                  {loading && <div>Loading...</div>}
+                  {suggestions.map(suggestion => {
+                    const className = suggestion.active
+                      ? 'suggestion-item--active'
+                      : 'suggestion-item';
+                    // inline style for demonstration purpose
+                    const style = suggestion.active
+                      ? { backgroundColor: '#007FFF', cursor: 'pointer' /*Hover*/}
+                      : { backgroundColor: '#ffffff', cursor: 'pointer',};
+                    return (
+                      <div
+                        {...getSuggestionItemProps(suggestion, {
+                          className,
+                          style,
+                        })}
+                      >
+                        <span>{suggestion.description}</span>
+                      </div>
+                    );
+                  })}
                 </div>
               </div>
+              <div className='main_card'></div>
+              <Cards/>
             </div>
           )}
         </PlacesAutocomplete>
