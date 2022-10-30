0e16f9b28767b4cb4cb5cab219b00f605ebbf7b8
ShubhangamPrasad <113502034+ShubhangamPrasad@users.noreply.github.com>
2022-10-30 16:17:49 +0800

    Updated to include dropdown

--- src/components/GoogleMap.js
+++ src/components/GoogleMap.js
@@ -6,6 +6,16 @@ import PlacesAutocomplete, {
 } from 'react-places-autocomplete';
 import './GoogleMap.css';
 import Cards from './Cards';
+import Dropdown1 from './Dropdowns/Dropdown1';
+import Dropdown2 from './Dropdowns/Dropdown2';
+import Dropdown3 from './Dropdowns/Dropdown3';
+import Dropdown4 from './Dropdowns/Dropdown4';
+import Dropdown5 from './Dropdowns/Dropdown5';
+import Dropdown6 from './Dropdowns/Dropdown6';
+import Dropdown7 from './Dropdowns/Dropdown7';
+import Dropdown8 from './Dropdowns/Dropdown8';
+import Dropdown9 from './Dropdowns/Dropdown9';
+
 
 
 export class MapContainer extends Component {
@@ -97,8 +107,9 @@ export class MapContainer extends Component {
             </div>
           )}
         </PlacesAutocomplete>
-
           </div>
+
+          
           {/* change the column to 70px */}
           <div class = 'column-70'> 
             <div className="map-container">
@@ -126,10 +137,38 @@ export class MapContainer extends Component {
 
               {/* create container for ammenities! */}
               <div className='amenities-container'>
+                <div className='Banner'>
+                  <h2>Nearby Ameneties</h2></div>
+                <div className='indiv-ameneties'>
+                  <div className='Transport'>
+                    <h3>Transport</h3>
+                    <div className='transport-1'>
+                      <Dropdown1></Dropdown1>
+                      <Dropdown2></Dropdown2>
+                      <Dropdown3></Dropdown3>
+                    </div>
+                  </div>
+                  <div className='Retail'>
+                    <h3>Retail</h3>
+                    <div className='transport-1'>
+                      <Dropdown4></Dropdown4>
+                      <Dropdown5></Dropdown5>
+                      <Dropdown6></Dropdown6>
+                    </div>
+                  </div>
+                  <div className='Services'>
+                    <h3>Services</h3>
+                    <div className='transport-1'>
+                      <Dropdown7></Dropdown7>
+                      <Dropdown8></Dropdown8>
+                      <Dropdown9></Dropdown9>
+                    </div>
+                  </div>
+                </div>
+
+                
 
                 {/* add ammnities here for responsive deisng */}
-                <p>The ammenities will go here and the page will still be responsive</p>  
-
                </div>
           </div> 
         </div>
@@ -143,6 +182,34 @@ export class MapContainer extends Component {
     )
   }
 }
+
+const dropdowns = document.querySelectorAll('.dropdown');
+dropdowns.forEach(dropdown => {
+  const select = dropdown.querySelector('.select');
+  const caret = dropdown.querySelector('.caret');
+  const menu = dropdown.querySelector('.menu');
+  const options = dropdown.querySelectorAll('.menu li');
+  const selected = dropdown.querySelector('.selected');
+
+  select.addEventListener('click',() => {
+    select.classList.toggle('select-clicked');
+    caret.classList.toggle('caret-rotate');
+    menu.classList.toggle('menu-open');
+  });
+
+  options.forEach(option => {
+    option.addEventListener('click', () => {
+      selected.innerText = option.innerText;
+      select.classList.remove('select-clicked');
+      caret.classList.remove('caret-rotate');
+      menu.classList.remove('menu-open');
+      options.forEach(option => {
+        option.classList.remove('active');
+      });
+      option.classList.add('active');
+    })
+  })
+})
 
 export default GoogleApiWrapper({
   apiKey: ('AIzaSyAJy82TVP3JryIg444dv-DfcDUnFoG-tt4')
